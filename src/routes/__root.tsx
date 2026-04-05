6cb11d43df0b2892020b9576673ee6266b60a3f9
Rhseung <ryu@rhseung.me>
2026-04-05 15:38:45 +0900

    feat: add error and not found pages with routing support

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,9 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router';
 
+import { ErrorPage, NotFoundPage } from '@/common/pages';
+
 export const Route = createRootRoute({
+  notFoundComponent: NotFoundPage,
+  errorComponent: ({ error, reset }) => <ErrorPage error={error} reset={reset} />,
   component: () => <Outlet />,
 });
