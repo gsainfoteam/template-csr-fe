a57a5bae7c3a53de96db5734e1ece748b417d00c
Rhseung <ryu@rhseung.me>
2026-04-09 15:21:13 +0900

    feat: replace manual language detection with i18next-browser-languagedetector

--- src/common/lib/i18n.ts
+++ src/common/lib/i18n.ts
@@ -1,4 +1,5 @@
 import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import HttpBackend from 'i18next-http-backend';
 import { initReactI18next, useTranslation } from 'react-i18next';
 
@@ -9,16 +10,12 @@ export function useLanguage(): Language {
   return i18nInstance.language as Language;
 }
 
-const browserLng = navigator.language.split('-')[0];
-const initialLng: Language = (LANGUAGES as readonly string[]).includes(browserLng)
-  ? (browserLng as Language)
-  : 'ko';
-
 await i18n
   .use(HttpBackend)
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    lng: initialLng,
+    supportedLngs: LANGUAGES,
     fallbackLng: 'ko',
     defaultNS: '_',
     nsSeparator: ':',
